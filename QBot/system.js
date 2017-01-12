d731470f7737b66fdabefab27254a2419fe3ab32
FALSEYING <false.ying@gmail.com>
2017-01-12 21:53:28 +0100

    Update system.js

--- QBot/system.js
+++ QBot/system.js
@@ -1,1 +1,298 @@
+//    COPYRIGHT»                         QPlug.cz 2017
+//    DEVELOPERS»                        FALSEYING
+//    ROOM»                              https://plug.dj/plugdj-czsk
+//    PRIKAZY»                           Doplníme..
 
+API.chatLog("[ QBot ] Systém je spustený!");
+var prefix = "!";
+
+//                          FUNKCIE                          //
+
+API.setVolume(0);
+API.sendChat("/cap 1");
+
+var emoji = $(".icon-emoji-on");
+if(emoji.length > 0){
+emoji[0].click();
+}
+
+var sounds = $(".icon-chat-sound-on");
+if(sounds.lenght > 0){
+sounds[0].click();  
+}
+
+var onlyaudio = $(".item.s-vo");
+if(onlyaudio.length > 0){
+onlyaudio[0].click();
+}
+
+var avatarsoff = $(".item.s-dancing.selected");
+if(avatarsoff.length > 0){
+avatarsoff[0].click();
+}
+
+var videooff = $(".item.s-av.selected");
+if(videooff.length > 0){
+videooff[0].click();
+}
+
+//                          AUTOWOOT                          //
+
+$('#woot').click();
+API.on(API.ADVANCE, advance);
+function advance(obj){
+$('#woot').click();
+}
+
+//                          BOUNCER                          //
+
+API.on(API.CHAT, bouncer);
+function bouncer(data){
+var msg = data.message;
+var fromid = data.uid;
+var from = data.un;
+
+var pinguin = "4006230";
+var jassyk = "4115729";
+var gyn = "3857912";
+var nyga = "3726868";
+var knedlik = "5014687";
+var samikk = "3653327";
+var atti = "13505684";
+var wrest = "4105532";
+var viktor = "4671474";
+var kevin = "4106134";
+
+if(msg === ""+ prefix +"bouncer"){
+if(fromid == pinguin || fromid == jassyk || fromid == gyn || fromid == nyga || fromid == knedlik || fromid == samikk ||  fromid == atti || fromid == wrest || fromid == viktor || fromid == kevin){
+API.sendChat("[@"+ from +"] Použil si Promote funkciu!");
+setTimeout(function(){ API.moderateSetRole(fromid, 2); }, 500);
+}
+else{
+API.sendChat("[@"+ from +"] Nemáš na to práva!");
+}
+}
+}
+
+API.on(API.CHAT, unbouncer);
+function unbouncer(data){
+var msg = data.message;
+var fromid = data.uid;
+var from = data.un;
+
+var pinguin = "4006230";
+var jassyk = "4115729";
+var gyn = "3857912";
+var nyga = "3726868";
+var knedlik = "5014687";
+var samikk = "3653327";
+var atti = "13505684";
+var wrest = "4105532";
+var viktor = "4671474";
+var kevin = "4106134";
+
+if(msg === ""+ prefix +"unbouncer"){
+if(fromid == pinguin || fromid == jassyk || fromid == gyn || fromid == nyga || fromid == knedlik || fromid == samikk ||  fromid == atti || fromid == wrest || fromid == viktor || fromid == kevin){
+API.sendChat("[@"+ from +"] Použil si Demote funkciu!");
+setTimeout(function(){ API.moderateSetRole(fromid, 0); }, 500);
+}
+else{
+API.sendChat("[@"+ from +"] Nemáš na to práva!");
+}
+}
+}
+
+//                          RESIDENT DJ                          //
+
+API.on(API.CHAT, rdj);
+function rdj(data){
+var msg = data.message;
+var fromid = data.uid;
+var from = data.un;
+
+var kebab = "5877483";
+var many = "";
+var lolly = "3845624";
+var sies = "5069104";
+var husmania = "3737749";
+
+if(msg === ""+ prefix +"rdj"){
+if(fromid == kebab || fromid == many || fromid == lolly || fromid == sies || fromid == husmania){
+API.sendChat("[@"+ from +"] Použil si Promote funkciu!");
+setTimeout(function(){ API.moderateSetRole(fromid, 1); }, 500);
+}
+else{
+API.sendChat("[@"+ from +"] Nemáš na to práva!");
+}
+}
+}
+
+API.on(API.CHAT, unrdj);
+function unrdj(data){
+var msg = data.message;
+var fromid = data.uid;
+var from = data.un;
+
+var kebab = "5877483";
+var many = "";
+var lolly = "3845624";
+var sies = "5069104";
+var husmania = "3737749";
+
+if(msg === ""+ prefix +"unrdj"){
+if(fromid == kebab || fromid == many || fromid == lolly || fromid == sies || fromid == husmania){
+API.sendChat("[@"+ from +"] Použil si Demote funkciu!");
+setTimeout(function(){ API.moderateSetRole(fromid, 0); }, 500);
+}
+else{
+API.sendChat("[@"+ from +"] Nemáš na to práva!");
+}
+}
+}
+
+//                          UZIVATELIA                          //
+
+API.on(API.CHAT, web);
+function web(data){
+var msg = data.message;
+var from = data.un;
+
+if(msg === ""+ prefix +"web" || msg === ""+ prefix +"stránka" || msg === ""+ prefix +"stranka" || msg === ""+ prefix +"page" || msg === ""+ prefix +"site"){
+API.sendChat("[ WEB ] Viac informácii o našej komunite!");
+}
+}
+
+API.on(API.CHAT, pravidla);
+function pravidla(data){
+var msg = data.message;
+var from = data.un;
+
+if(msg === ""+ prefix +"pravidlá" || msg === ""+ prefix +"pravidla" || msg === ""+ prefix +"rules"){
+API.sendChat("[ PRAVIDLÁ ] Pravidlá našej komunita najdeš na!");
+}
+}
+
+API.on(API.CHAT, facebook);
+function facebook(data){
+var msg = data.message;
+var from = data.un;
+
+if(msg === ""+ prefix +"facebook" || msg === ""+ prefix +"fb"){
+API.sendChat("[ FACEBOOK ] Hoď like na našu Facebook stránku https://bit.ly/QPlugcz!");
+}
+}
+
+API.on(API.CHAT, autowoot);
+function autowoot(data){
+var msg = data.message;
+var from = data.un;
+
+if(msg === ""+ prefix +"autowoot" || msg === ""+ prefix +"aw" || msg === ""+ prefix +"rcs"){
+API.sendChat("[ AUTOWOOT ] Ak chcete vidieť naše pozadie komunity použite https://rcs.radiant.dj/install!");
+}
+}
+
+API.on(API.CHAT, prikazy);
+function prikazy(data){
+var msg = data.message;
+var from = data.un;
+var fromid = data.uid;
+
+if(msg === ""+ prefix +"prikazy" || msg === ""+ prefix +"commands"){
+API.sendChat("[ PRÍKAZY ] Príkazy našeho Systému najdeš na!");
+}
+}
+
+API.on(API.CHAT, nabor);
+function nabor(data){
+var msg = data.message;
+var from = data.un;
+var fromid = data.uid;
+
+if(msg === ""+ prefix +"nabor" || msg === ""+ prefix +"nábor"){
+API.sendChat("[ NÁBOR ] Aktuálne prebieha Nábor do týmu, ktorý najdeš na!");
+}
+}
+
+API.on(API.CHAT, pd);
+function pd(data){
+var msg = data.message;
+var from = data.un;
+
+if(msg === ""+ prefix +"pd" || msg === ""+ prefix +"p&d"){
+API.sendChat("[ INFO ] Promote&Demote funckia je len pre náš akutálny Staff Tým. Bouncery a Resident DJi si sami môžu vymazať práva a zasa si ich vrátiť. Príkazy pre Vyhadzovačov sú !bouncer a !unbouncera a pre Residentov sú !rdj a !unrdj.");
+}
+}
+
+API.on(API.CHAT, event);
+function event(data){
+var msg = data.message;
+var from = data.un;
+
+if(msg === ""+ prefix +"event" || msg === ""+ prefix +"sútaž" || msg === ""+ prefix +"súťaž"){
+API.sendChat("[@"+ from +"] Momentálne sa nechystá žiadny event. Ak chceš vedieť kedy sa bude konať Event ako prvý hoď Like na Facebook: https://bit.ly/QPlugcz & Stránka:!");
+//API.sendChat("NÁZOV: - | DÁTUM: x.x.2017 od xx:xx do xx:xx | http://musicforeveryone.6f.sk/eventy");
+}
+}
+
+API.on(API.CHAT, výhra);
+function výhra(data){
+var msg = data.message;
+var from = data.un;
+
+if(msg === ""+ prefix +"výhra" || msg === ""+ prefix +"vyhra" || msg === ""+ prefix +"odmena"){
+API.sendChat("[@"+ from +"] Momentálne sa nechystá žiadny event. Ak chceš vedieť kedy sa bude konať Event ako prvý hoď Like na Facebook: https://bit.ly/QPlugcz & Stránka:!");
+//API.sendChat("1. MIESTO: - | 2: - | 3: - | 4: -");
+}
+}
+
+API.on(API.CHAT, afk);
+function afk(data){
+var msg = data.message;
+var from = data.un;
+
+if(msg === ""+ prefix +"afk" || msg === ""+ prefix +"away" || msg === ""+ prefix +"preč" || msg === ""+ prefix +"prec"){
+API.sendChat("[ AFK ] Užívateľ @"+ from +" je práve preč!");
+}
+}
+
+API.on(API.CHAT, back);
+function back(data){
+var msg = data.message;
+var from = data.un;
+
+if(msg === ""+ prefix +"back" || msg === ""+ prefix +"here"){
+API.sendChat("[ AFK ] Užívateľ @"+ from +" sa práve vrátil!");
+}
+}
+
+API.on(API.CHAT, joinwl);
+function joinwl(data){
+var msg = data.message;
+var from = data.uid;
+
+if(msg === ""+ prefix +"joinwl" || msg === ""+ prefix +"pripojit"){
+API.moderateAddDJ(from);
+}
+}
+
+API.on(API.CHAT, leavewl);
+function leavewl(data){
+var msg = data.message;
+var from = data.uid;
+
+if(msg === ""+ prefix +"leavewl" || msg === ""+ prefix +"odpojit"){
+API.moderateRemoveDJ(from);
+}
+}
+
+API.on(API.CHAT, id);
+function id(data){
+var msg = data.message;
+var from = data.un;
+var fromid = data.uid;
+
+if(msg === ""+ prefix +"id"){
+API.sendChat("[@"+ from +"] Tvoje ID je: "+ fromid +".");
+}
+}
